7ff3854a3980e56779c54c9c4dfac42577240b04
gosoccerboy5 <82768218+gosoccerboy5@users.noreply.github.com>
2021-06-02 10:01:02 -0500

    all right lets get this going

--- resources/index.js
+++ resources/index.js
@@ -1,11 +1,31 @@
-const base = document.createElement("base");
-base.setAttribute("href", "https://gosoccerboy5.github.io/blog/");
-document.head.append(base);
-let icon = document.createElement("link");
-icon.setAttribute("rel", "icon");
-icon.setAttribute("href", "/resources/favicon.ico");
-document.head.append(icon);
-let stylesheet = document.createElement("link");
-stylesheet.setAttribute("rel", "stylesheet");
-stylesheet.setAttribute("href", "/resources/stylesheet.css");
-document.head.append(stylesheet);
+{
+    let meta = document.createElement("meta");
+    meta.setAttribute("charset", "UTF-8");
+    document.head.append(meta);
+}
+{
+    let base = document.createElement("base");
+    base.href = "https://gosoccerboy5.github.io/blog/";
+    document.head.append(base);
+}
+{
+    let icon = document.createElement("link");
+    icon.rel = "icon";
+    icon.href = "resources/favicon.ico";
+    document.head.append(icon);
+}
+{
+    let stylesheet = document.createElement("link");
+    stylesheet.rel = "stylesheet";
+    stylesheet.href = "resources/stylesheet.css";
+    document.head.append(stylesheet);
+}
+{
+    let homeLink = document.createElement("a");
+    homeLink.href = "";
+    homeLink.id = "home";
+    homeLink.textContent = "Home";
+    window.onload = function() {
+        document.body.append(document.createElement("br"), document.createElement("br"), homeLink);
+    };
+}
